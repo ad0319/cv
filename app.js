ce59880a12c9a6c103ae40c25be103be381a0cc5
ad0319 <103395227+ad0319@users.noreply.github.com>
2022-04-19 16:23:09 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function checkNum(){
+ var Y = parseInt(document.getElementById("A").value)
+if(Y,X > String || Y,X < String || isNaN(Y,X)){ 
+     alert("请输入数字");
+     document.getElementById('A').value = ""; }
+ var X=document.getElementById ("B").value 
+   var demoP=document.getElementById("demo");
+    
+    demoP.innerHTML= Number(Y) + Number(X)}
